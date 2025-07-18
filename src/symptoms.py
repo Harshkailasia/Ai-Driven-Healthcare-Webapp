f9c73eb06832ef9e3781483fdb316b9e624c8953
Avnish <160031022+Avnish1447@users.noreply.github.com>
2025-07-18 16:09:11 +0530

    🌡️Update symptoms.py

--- src/symptoms.py
+++ src/symptoms.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 
 # Load dataset
-data = pd.read_csv("Training.csv")
+data = pd.read_csv("data/Training.csv")
 AVAILABLE_SYMPTOMS = data.columns[:-1].tolist()
